8c37bf24050ee5c0a4f3a93b81e75716203aaa8a
Sathish P <146912497+Sathishraan@users.noreply.github.com>
2025-08-20 09:36:15 +0530

    server update

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,15 +1,14 @@
-import exprsess from 'express';
+// routes/messageRoutes.js - FIXED VERSION
+
+import express from 'express';  // ✅ Fixed typo: was "exprsess"
 import { getMessage, getusersForSidebar, markMessageAsSeen, sendMessage } from '../controllers/messageController.js';
 import { protectRoute } from '../middleware/auth.js';
 
+const messageRouter = express.Router();  // ✅ Fixed: was "exprsess.Router()"
 
-
-
-const messageRouter = exprsess.Router();
-
-messageRouter.get('/users',protectRoute, getusersForSidebar  )
-messageRouter.get('/:id',protectRoute, getMessage )
-messageRouter.put('/mark/:id',protectRoute, markMessageAsSeen )
-messageRouter.post('/send/:id', protectRoute,sendMessage )
+messageRouter.get('/users', protectRoute, getusersForSidebar);
+messageRouter.get('/:id', protectRoute, getMessage);
+messageRouter.put('/mark/:id', protectRoute, markMessageAsSeen);
+messageRouter.post('/send/:id', protectRoute, sendMessage);
 
 export default messageRouter;
